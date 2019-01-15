236d5c19a3e2d1d4a8372ab94b35b7c9cfeb6071
Adelinestack <adeline.colombo@stackerine.com>
2019-01-15 11:06:27 +0100

    commit app Stepper with one function

--- projet-react/src/components/Stepper.js
+++ projet-react/src/components/Stepper.js
@@ -8,12 +8,7 @@ class Stepper extends Component {
       counter: 0,
     };
   }
-  minus(num) {
-    this.setState({
-      counter: this.state.counter - num,
-    });
-  }
-  plus(num) {
+  add(num) {
     this.setState({
       counter: this.state.counter + num,
     });
@@ -23,13 +18,13 @@ class Stepper extends Component {
     return (
       <div className="container">
         <div>
-          <button onClick={() => this.minus(10)}>-10</button>
-          <button onClick={() => this.minus(1)}>-1</button>
+          <button onClick={() => this.add(-10)}>-10</button>
+          <button onClick={() => this.add(-1)}>-1</button>
         </div>
         <div className="counter">{this.state.counter}</div>
         <div>
-          <button onClick={() => this.plus(1)}>+1</button>
-          <button onClick={() => this.plus(10)}>+10</button>
+          <button onClick={() => this.add(1)}>+1</button>
+          <button onClick={() => this.add(10)}>+10</button>
         </div>
       </div>
     );
